0c142eab92b6ec724e0380ae920e07046b1e36cd
Linh Nguyen <linh.nguyen.it91@gmail.com>
2019-11-22 02:42:40 +0700

    init

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "EDA YouTube channels in Vietnam",
+        "excerpt":"Table of content 1&nbsp;&nbsp;EDA YouTube channels in Vietnam1.1&nbsp;&nbsp;Goals of the analysis1.2&nbsp;&nbsp;General analysis1.2.1&nbsp;&nbsp;Channels with most subscribers.1.2.2&nbsp;&nbsp;Number of subscribers vs. views1.2.3&nbsp;&nbsp;Number of videos vs. views1.2.4&nbsp;&nbsp;Category per channels1.2.5&nbsp;&nbsp;Channel which still keep up or thrive fast in the short time.1.2.6&nbsp;&nbsp;Channels with less video but great views1.3&nbsp;&nbsp;More specific on Entertainment1.4&nbsp;&nbsp;More specific on Eduction1.5&nbsp;&nbsp;Keywords1.5.1&nbsp;&nbsp;Wordcloud1.6&nbsp;&nbsp;Conclusion1.6.1&nbsp;&nbsp;Benefits.1.6.2&nbsp;&nbsp;About the content,...","categories": ["EDA"],
+        "tags": ["EDA","Data Mining","Data Analysis"],
+        "url": "https://wakemei.com/eda/channels/",
+        "teaser":null}]
